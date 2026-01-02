c206213051e4f1e8c121e8969e7f1651efd55333
kjbeath <ken@kjbeath.id.au>
2026-01-02 16:02:43 +1100

    Add files via upload

---
+++ analysebackblaze.R
@@ -0,0 +1,72 @@
+library(survival)
+library(biostat3)
+library(tidyverse)
+
+sumdata <- read.csv("sumbackblaze.csv")
+
+theyears <- as.data.frame(table(sumdata$year))
+
+sumdata <- sumdata %>%
+  dplyr::mutate(model = if_else(model=="WDC  WUH721414ALE6L4","WDC WUH721414ALE6L4", model)) %>%
+  dplyr::mutate(manuf=str_sub(model,1,2),
+        manuf = if_else(manuf %in% c("HG","Hi", "ST","TO","WD"), manuf, "OT"),
+manuf = factor(manuf, levels=c("HG","Hi", "ST","TO","WD","OT"), labels=c("HGST","Hitachi","Seagate","Toshiba","Western Digital","Other")),
+manuf = as.character(manuf)) %>%
+  dplyr::mutate(groupyear = factor(year))
+
+disks.fityear <- survfit(Surv(time,status)~groupyear, data=sumdata)
+plot(disks.fityear, xlab="Time (years)", ylab="Cumulative Proportion with Failure",
+     conf.int=FALSE,
+     xmax=10, col=rainbow(nlevels(sumdata$groupyear)), lty=1, lwd=2, fun="event",
+     main=paste("Cumulative Failures for Drives by Starting Year"))
+
+legend("topleft", levels(sumdata$groupyear), lty=1, lwd=2, col = rainbow(nlevels(sumdata$groupyear)))
+
+fit2 <- muhaz2(Surv(time, status)~groupyear, max.time=min(4,max(sumdata$time)), data=sumdata[sumdata$year <=2023,])
+
+plot(fit2, col=rainbow(nlevels(sumdata$groupyear)), lwd=2, lty=1, 
+     xlim=c(0,4),
+     legend.args=list(legend=levels(sumdata$groupyear),x="topleft",lwd=2,
+                      col=rainbow(nlevels(sumdata$groupyear))),
+     main=paste("Hazard for Drives Installed by Year \nMore than 2000 drives installed"))
+
+
+for (startyear in min(sumdata$year):(max(sumdata$year)-2)) {
+  
+  nodrives <- as.data.frame(table(sumdata$model[sumdata$year==startyear]))
+  names(nodrives) <- c("model","nodrives")
+  
+  sumdata2 <- merge(sumdata,nodrives)
+  sumdata2 <- sumdata2[(sumdata2$year==startyear) & (sumdata2$nodrives>2000),]
+  sumdata2$model <- factor(sumdata2$model)
+  #browser()
+  disks.fitall <- survfit(Surv(time,status)~model, data=sumdata2)
+  plot(disks.fitall, xlab="Time (years)", ylab="Cumulative Proportion with Failure",
+       conf.int=FALSE,
+       xmax=floor(min(4,max(sumdata2$time))), col=rainbow(nlevels(sumdata2$model)), lty=1, lwd=2, fun="event",
+       main=paste("Cumulative Failures for Drives Installed in ", startyear,"\nMore than 2000 drives installed"))
+       
+  legend("topleft", levels(sumdata2$model), lty=1, lwd=2, col = rainbow(nlevels(sumdata2$model)))
+}
+
+for (startyear in min(sumdata$year):(max(sumdata$year)-3)) {
+#for (startyear in 2021:(max(sumdata$year)-4)) {
+    
+  nodrives <- as.data.frame(table(sumdata$model[sumdata$year==startyear]))
+  names(nodrives) <- c("model","nodrives")
+  
+  sumdata2 <- merge(sumdata,nodrives)
+  sumdata2 <- sumdata2[(sumdata2$year==startyear) & (sumdata2$nodrives>2000),]
+  sumdata2$model <- factor(sumdata2$model)
+  #browser()
+#  disks.fitall <- survfit(Surv(time,status)~model, data=sumdata2)
+#  if (startyear==2021) browser()
+  fit2 <- muhaz2(Surv(time, status)~model, max.time=min(4,max(sumdata2$time)), data=sumdata2)
+#  fit2 <- muhaz2(Surv(time, status)~model, data=sumdata2)
+#  browser()
+  plot(fit2, col=rainbow(nlevels(sumdata2$model)), lwd=2, lty=1, 
+       xlim=c(0,4),
+       legend.args=list(legend=levels(sumdata2$model),x="topleft",lwd=2,
+                        col=rainbow(nlevels(sumdata2$model))),
+       main=paste("Hazard for Drives Installed in ", startyear,"\nMore than 2000 drives installed"))
+}
---
+++ buildbackblaze.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(survival)
+library(muhaz)
+
+dir.list <- list.files(path = "./data", pattern = NULL, all.files = FALSE,
+                        full.names = TRUE, recursive = FALSE,
+                        ignore.case = FALSE, include.dirs = TRUE, no.. = FALSE)
+
+
+# first summarise the data in each period for each drive with just start and end date in that period
+dataset <- do.call("rbind", lapply(dir.list, FUN = function(thedir) {
+  file.list <- list.files(path = thedir , pattern = NULL, all.files = FALSE,
+                          full.names = TRUE, recursive = TRUE,
+                          ignore.case = FALSE, include.dirs = FALSE, no.. = FALSE)
+    temp <- do.call("rbind", lapply(file.list, FUN = function(thefile) {
+       print(thefile)
+         temp <- read.csv(thefile)[,1:5]
+        temp$date <- lubridate::as_date(temp$date)
+        temp
+    }))
+    temp <- temp %>%
+      group_by(serial_number, model) %>%
+      summarise(firstdate =min(date), lastdate=max(date), status=max(failure), capacity_bytes=max(capacity_bytes))
+    temp
+ }))
+
+# now combine all the periods to have start and end date
+# and convert periods to years
+
+sumdata <- dataset %>%
+  group_by(serial_number, model) %>%
+  summarise(firstdate =min(firstdate), lastdate=max(lastdate), status=max(status), capacity_bytes=max(capacity_bytes)) %>%
+  dplyr::mutate(time=as.numeric(lastdate-firstdate)/365.25) %>%
+  dplyr::mutate(year=lubridate::year(firstdate))
+
+write.csv(sumdata,file="sumbackblaze.csv")
