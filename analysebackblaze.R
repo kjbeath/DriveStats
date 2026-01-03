23d20f468e323a6d4d1e49517254a0396b608c77
Ken Beath <kenbeath@Kens-MacBook-Air-2.local>
2026-01-03 17:25:54 +1100

    Tidy and improve analysis program.

--- analysebackblaze.R
+++ analysebackblaze.R
@@ -10,10 +10,12 @@ sumdata <- sumdata %>%
   dplyr::mutate(model = if_else(model=="WDC  WUH721414ALE6L4","WDC WUH721414ALE6L4", model)) %>%
   dplyr::mutate(manuf=str_sub(model,1,2),
         manuf = if_else(manuf %in% c("HG","Hi", "ST","TO","WD"), manuf, "OT"),
-manuf = factor(manuf, levels=c("HG","Hi", "ST","TO","WD","OT"), labels=c("HGST","Hitachi","Seagate","Toshiba","Western Digital","Other")),
+manuf = factor(manuf, levels=c("HG","Hi", "ST","TO","WD","OT"),
+               labels=c("HGST","Hitachi","Seagate","Toshiba","Western Digital","Other")),
 manuf = as.character(manuf)) %>%
   dplyr::mutate(groupyear = factor(year))
 
+# survival for all disks by year
 disks.fityear <- survfit(Surv(time,status)~groupyear, data=sumdata)
 plot(disks.fityear, xlab="Time (years)", ylab="Cumulative Proportion with Failure",
      conf.int=FALSE,
@@ -35,9 +37,22 @@ for (startyear in min(sumdata$year):(max(sumdata$year)-2)) {
   
   nodrives <- as.data.frame(table(sumdata$model[sumdata$year==startyear]))
   names(nodrives) <- c("model","nodrives")
+
+  # remove models with no failures  
+  hasfailures <- sumdata %>%
+    filter(year==startyear)  %>%
+    dplyr::select(model, status) %>%
+    group_by(model) %>%
+    summarise(hasfailures=max(status))
+
+  print("no failures")
+  print(hasfailures$model[hasfailures$hasfailures==0]) 
   
   sumdata2 <- merge(sumdata,nodrives)
-  sumdata2 <- sumdata2[(sumdata2$year==startyear) & (sumdata2$nodrives>2000),]
+  sumdata2 <- merge(sumdata2,hasfailures)
+  
+  sumdata2 <- sumdata2[(sumdata2$year==startyear) & (sumdata2$nodrives>2000)
+                       & (sumdata2$hasfailures>0),]
   
 
   sumdata2$model <- factor(sumdata2$model)
@@ -56,17 +71,25 @@ for (startyear in min(sumdata$year):(max(sumdata$year)-3)) {
     
   nodrives <- as.data.frame(table(sumdata$model[sumdata$year==startyear]))
   names(nodrives) <- c("model","nodrives")
-  
+
+  # remove models with no failures  
+  hasfailures <- sumdata %>%
+    filter(year==startyear)  %>%
+    dplyr::select(model, status) %>%
+    group_by(model) %>%
+    summarise(hasfailures=max(status))
+ 
   sumdata2 <- merge(sumdata,nodrives)
-  sumdata2 <- sumdata2[(sumdata2$year==startyear) & (sumdata2$nodrives>2000),]
+  sumdata2 <- merge(sumdata2,hasfailures)
+  
+  sumdata2 <- sumdata2[(sumdata2$year==startyear) & (sumdata2$nodrives>2000)
+                       & (sumdata2$hasfailures>0),]
+  
   sumdata2$model <- factor(sumdata2$model)
-  #browser()
-#  disks.fitall <- survfit(Surv(time,status)~model, data=sumdata2)
-#  if (startyear==2021) browser()
-  fit2 <- muhaz2(Surv(time, status)~model, max.time=min(4,max(sumdata2$time)), data=sumdata2)
-#  fit2 <- muhaz2(Surv(time, status)~model, data=sumdata2)
-#  browser()
-  plot(fit2, col=rainbow(nlevels(sumdata2$model)), lwd=2, lty=1, 
+
+    fit2 <- muhaz2(Surv(time, status)~model, max.time=min(4,max(sumdata2$time)), data=sumdata2)
+
+      plot(fit2, col=rainbow(nlevels(sumdata2$model)), lwd=2, lty=1, 
        xlim=c(0,4),
        legend.args=list(legend=levels(sumdata2$model),x="topleft",lwd=2,
                         col=rainbow(nlevels(sumdata2$model))),
